8cb2b952ca43dc2d6dba49846eb637dbd5870889
jackhou2007 <jackhou2007@gmail.com>
2014-06-28 23:31:42 +0800

    add class method

--- src/dom.js
+++ src/dom.js
@@ -13,5 +13,25 @@
         return parent.getElementById(id);
     };
 
+    // get elements by classname
+    dom.getByClass = function (classname, parent) {
+        var oParent = parent ? document.getElementById(parent) : document,
+            eles = [],
+            elements = oParent.getElementsByTagName("*");
+
+        for (var i = 0, len = elements.length; i < len; i++) {
+            if (classReg(elements[i].className).test(classname)) {
+                eles.push(elements[i]);
+            }
+        }
+
+        return eles;
+    }
+
+    // function helper
+    function classReg(classname) {
+        return new RegExp("(^|\\s)" + classname + "(\\s|$)");
+    }
+
     window.dom = dom;
 }) (window, document);
